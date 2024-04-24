7b3f7cf92eaeda2c7731b5db31f9a5e6c39eb505
Carolina Palacios <caaro.palacios@hotmail.com>
2024-04-24 19:16:52 -0300

    fix/setup&consts

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,7 +10,7 @@ import {
 } from '../views/index'
 
 export const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VUE_APP_BASE_URL),
   routes: [
     {
       path: '/',
